71a9b1dda6bf919dbc8e5a7a9a7524377efbdf32
ruumiia <78075902+ruumiia@users.noreply.github.com>
2021-12-28 01:06:04 +0800

    updated query code and form requirements, added comments

--- editstudent.php
+++ editstudent.php
@@ -23,15 +23,18 @@
 
       <?php 
       
-      include "sql-db-connect.php";
-
-      $studentnum = $_POST['StudentNum'];
+      include "sql-db-connect.php"; // php file to connect to database 
+      
+      $id = $_GET['id'];
   
+      //query part to take row with equivalent student number for edit
+
       $sql = "SELECT * FROM `tblstudent` 
-      WHERE `fldstudentno` = $studentnum";
+      WHERE `fldindex` = $id";
       $result = $con->query($sql);
       if ($result->num_rows > 0) {
-      // output data of each row
+        
+      // outputs the row data with fldindex equal to the associated id 
       while($row = $result->fetch_assoc()) {
 
       ?>
@@ -41,28 +44,28 @@
       <tr>
       <th>Student No.:</th>
       <input type='hidden' value ='<?php echo $row['fldindex']?>' name="index" />
-      <th><input type='text' name='StudentNum' value="<?php echo $row["fldstudentno"] ?>" placeholder='Student Number'></th>
+      <th><input type='text' name='StudentNum' value="<?php echo $row["fldstudentno"] ?>" placeholder='Student Number' required></th>
       </tr>
       <tr>
       <th>Last Name:</th>
-      <th><input type='text' name='LastName' value="<?php echo $row["fldlastname"] ?>" placeholder='Last Name'></th>
+      <th><input type='text' name='LastName' value="<?php echo $row["fldlastname"] ?>" placeholder='Last Name' required></th>
       </tr>
       <tr>
       <th>First Name:</th>
-      <th><input type='text' name='FirstName' value="<?php echo $row["fldfirstname"] ?>" placeholder='First Name'></th>
+      <th><input type='text' name='FirstName' value="<?php echo $row["fldfirstname"] ?>" placeholder='First Name' required></th>
       </tr>
       <tr>
       <th>Middle Name:</th>
-      <th><input type='text' name='MiddleName' value="<?php echo $row["fldmiddlename"] ?>" placeholder='Middle Name'></th>
+      <th><input type='text' name='MiddleName' value="<?php echo $row["fldmiddlename"] ?>" placeholder='Middle Name' required></th>
       </tr>
       <tr>
       <th>Gender:</th>
-      <th><input type='text' name='Gender' value="<?php echo $row["fldgender"] ?>" placeholder='Gender'></th>
+      <th><input type='text' name='Gender' value="<?php echo $row["fldgender"] ?>" placeholder='Gender' required></th>
       </tr>
       </table>
       <div class = 'labelcontainer '>
       <input type='submit' class ='bottombuttons' name='updatebutton' value='Update Student' onclick="action='editstudent-sql.php'" /> 
-      <input type='submit' value='Cancel' onclick="action='menu.php'" />
+      <input type='submit' value='Cancel' onclick="action='menu.php'" formnovalidate/>
       </div> 
       </form>
       <?php
@@ -73,5 +76,3 @@
 
 </body>
 </html>
-
-    
