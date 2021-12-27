fe99858f06a08af7e100440e00a10139c9914a2c
ruumiia <78075902+ruumiia@users.noreply.github.com>
2021-12-27 20:44:02 +0800

    updated addstudent-sql.php function
    added code to check whether the student number input already exists within the database, and added conditions to satisfy the check results
    added comments to elaborate on parts

--- addstudent-sql.php
+++ addstudent-sql.php
@@ -1,12 +1,31 @@
 <?php
 
-include "sql-db-connect.php";
-
+include "sql-db-connect.php"; // php file to connect to database
+      
       $studentnum = $_POST['StudentNum'];
       $lastname = $_POST['LastName'];
       $firstname = $_POST['FirstName'];
       $middlename = $_POST['MiddleName'];
       $gender = $_POST['Gender'];
+      $count = 0;
+
+      $sqlsearch = "SELECT * FROM `tblstudent` WHERE `fldstudentno` = $studentnum";
+
+      $result = mysqli_query($con,$sqlsearch);
+
+      while($row = mysqli_fetch_assoc($result)) { // if sql table has equal student number to input, count++
+        if ($row['fldstudentno'] == $studentnum) {
+            $count++;
+        }
+      }
+
+      //if count > 0, student number already exists in the db - retry; else, input added to db
+      if ($count > 0) {
+        echo "<script>alert('Student Number $studentnum already exists. Please retry.')
+        location.href = 'addstudent.php?attempt=success';
+        </script>"; 
+      }
+      else {
 
       $sql = "INSERT INTO tblstudent (fldstudentno, fldlastname, fldfirstname, fldmiddlename, fldgender) 
       VALUES 
@@ -23,7 +42,5 @@ include "sql-db-connect.php";
         {
             echo "Error adding user in database<br />";
         }
+    }
 ?>	
-
-
-
