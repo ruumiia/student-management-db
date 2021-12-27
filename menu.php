27b5dc3f48f1b3b000da379656b95162f2c74a27
ruumiia <78075902+ruumiia@users.noreply.github.com>
2021-12-28 01:17:41 +0800

    updated menu content
    shortened menu code (php functions moved to search.php file instead)
    added some comment descriptions

--- menu.php
+++ menu.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,56 +20,25 @@
 
   <div class="column right">
     </br>
+    <div class ="tablediv">
     <div class = "labelcontainer"><p class ="label">Student</p></div>
-    
-    <form action = "addstudent.php" method = "post" enctype="multipart/form-data">
-    <input type="submit" name="submit" class = "addstudent" value="Add Student">
+    <form method = "post" class = "topOptions" enctype="multipart/form-data">
+    <input type="text" name="searchinput" required>
+    <input type="submit" name="searchbutton" value="Search Student" onclick="action='menu.php'; return true;" />
+    <input type="submit" name="submit" onclick="action='addstudent.php'; return true;" value="Add Student" formnovalidate >
     </form>
+    
     <?php
+      include "search.php"; // including php file for printing the sql table and search results for every menu.php direct
+    ?>  
 
-      include "sql-db-connect.php";
-    
-      $result = mysqli_query($con,"SELECT * FROM tblstudent");
+    </div>
+        <div class = "labelcontainer bottomdiv">  <!-- div container for display all records option-->
+        <form action="" method="post" class = "displayrecordbutton"> 
+        <input type="submit" name="displayallbutton" value="Display All Record" />
+        </form>
+        </div>
 
-      echo "<table class='mysqltable'>
-      <tr>
-      <th>flindex</th>
-      <th>fldstudentno</th>
-      <th>fldlastname</th>
-      <th>fldfirstname</th>
-      <th>fldmiddlename</th>
-      <th>fldgender</th>
-      </tr>";
-
-      while($row = mysqli_fetch_array($result))
-      {
-      echo "<tr>";
-      echo "<td>" . $row['fldindex'] . "</td>";
-      echo "<td>" . $row['fldstudentno'] . "</td>";
-      echo "<td>" . $row['fldlastname'] . "</td>";
-      echo "<td>" . $row['fldfirstname'] . "</td>";
-      echo "<td>" . $row['fldmiddlename'] . "</td>";
-      echo "<td>" . $row['fldgender'] . "</td>";
-      echo "</tr>";
-      }
-      echo "</table>";
-
-      mysqli_close($con);
-    ?>
-  
-  <div class = "labelcontainer">
-    
-  <p class ="label2">Type Student Number:</p>
-
-  <form action="" method="post" class = "editdelstudent"> 
-  <input type="text" name="StudentNum" placeholder="Student Number" required="">
-  <input type="submit" name="editbutton" value="Edit Student" onclick="action='editstudent.php'; return true;" />
-  <input type="submit" name="deletebutton" value="Delete Student" onclick="action='deletestudent.php'; return true;" />
-  </form>
-  
-</div>
-
-  </div>
 </div>
 
 </body>
